320c916944ebfb287ca1b608ca1a9b43b5b3b270
fatimaamaan58-afk <fatimaamaan58@gmail.com>
2026-05-07 18:09:36 +0300

    add GanttEntry Class

---
+++ src/GanttEntry.java
@@ -0,0 +1,10 @@
+public class GanttEntry {
+    String pid;
+    int start, end;
+
+    GanttEntry(String pid, int start, int end){
+        this.pid=pid;
+        this.start=start;
+        this.end=end;
+    }
+}
