caee7e996cfd397b455e819fcb0349d208ac685f
SaifAllah Ibrahim <saifossman2006@gmail.com>
2026-05-10 19:47:45 +0300

    comparison and algorithms have been added

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -6,9 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.control.TextArea;
 
 public class MainGUI extends Application {
     ObservableList<Process> processes = FXCollections.observableArrayList();
+    TextArea output = new TextArea();
     @Override
     public void start(Stage stage) {
         Label title = new Label("OS Scheduling GUI");
@@ -36,6 +38,7 @@ public class MainGUI extends Application {
         c4.setCellValueFactory(x -> new javafx.beans.property.SimpleIntegerProperty(x.getValue().priority));
 
         table.getColumns().addAll(c1, c2, c3, c4);
+        output.setPrefHeight(200);
         Button add = new Button("Add");
         add.setOnAction(e -> {
             try {
@@ -56,8 +59,91 @@ public class MainGUI extends Application {
         clear.setOnAction(e -> processes.clear());
 
         Button run = new Button("Run");
+        run.setOnAction(e -> {
+            if(processes.isEmpty()) {
+                output.setText("No Processes Added");
+                return;
+            }
+            java.util.List<Process> copied =
+                    InputHandler.copy(processes);
+            java.util.List<GanttEntry> result;
+            java.util.List<Process> priorityList =
+                    InputHandler.copy(processes);
+
+            java.util.List<Process> srtfList =
+                    InputHandler.copy(processes);
+
+            java.util.List<GanttEntry> priorityResult =
+                    PriorityScheduler.run(priorityList);
+
+            java.util.List<GanttEntry> srtfResult =
+                    SRTFScheduler.run(srtfList);
+
+            MetricsCalculator.calculate(priorityList);
+
+            MetricsCalculator.calculate(srtfList);
+
+            double[] priorityAvg =
+                    MetricsCalculator.getAverages(priorityList);
+
+            double[] srtfAvg =
+                    MetricsCalculator.getAverages(srtfList);
+            MetricsCalculator.calculate(copied);
+            double[] avg =
+                    MetricsCalculator.getAverages(copied);
+            StringBuilder text = new StringBuilder();
+
+            text.append("         PRIORITY          \n\n");
+
+            for(GanttEntry g : priorityResult) {
+
+                text.append(g.pid)
+                        .append(" : ")
+                        .append(g.start)
+                        .append(" -> ")
+                        .append(g.end)
+                        .append("\n");
+
+            }
+
+            text.append("\nAverage WT: ")
+                    .append(priorityAvg[0]);
+
+            text.append("\nAverage TAT: ")
+                    .append(priorityAvg[1]);
+
+            text.append("\nAverage RT: ")
+                    .append(priorityAvg[2]);
+
+            text.append("\n\n           \n\n");
+
+            text.append("       SRTF       \n\n");
+
+            for(GanttEntry g : srtfResult) {
+
+                text.append(g.pid)
+                        .append(" : ")
+                        .append(g.start)
+                        .append(" -> ")
+                        .append(g.end)
+                        .append("\n");
+
+            }
+
+            text.append("\nAverage WT: ")
+                    .append(srtfAvg[0]);
+
+            text.append("\nAverage TAT: ")
+                    .append(srtfAvg[1]);
+
+            text.append("\nAverage RT: ")
+                    .append(srtfAvg[2]);
+
+            output.setText(text.toString());
+        });
+
         HBox input = new HBox(10, pid, at, bt, pr, add, clear, run);
-        VBox root = new VBox(15, title, input, table);
+        VBox root = new VBox(15, title, input, table, output);
         root.setPadding(new Insets(10));
         stage.setScene(new Scene(root, 800, 500));
         stage.setTitle("OS Project");
